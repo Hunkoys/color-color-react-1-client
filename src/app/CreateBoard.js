06246292a9ac4f26fe70017eb0f81906cf79ddd0
Hunkoys <hunkyminik@gmail.com>
2021-02-18 01:15:39 -0800

    Minor changes

--- src/app/CreateBoard.js
+++ src/app/CreateBoard.js
@@ -1,6 +1,8 @@
+import goto from './goto';
 import Screen from './Screen';
-import Button from './screen/Button';
 import Card from './screen/Card';
+import Button from './screen/Button';
+import Splash from './Splash';
 
 export default class CreateBoard extends Screen {
   render() {
@@ -10,6 +12,7 @@ export default class CreateBoard extends Screen {
         <Button>7 x 7</Button>
         <Button>11 x 11</Button>
         <Button>15 x 15</Button>
+        <Button action={() => goto(Splash)}>Home</Button>
       </Card>
     );
   }
--- src/app/Splash.js
+++ src/app/Splash.js
@@ -14,13 +14,12 @@ export default class Splash extends Screen {
   }
 
   render() {
-    console.log(this.state.username);
     return (
       <Card>
         <h1>Color Color</h1>
         <TextBox store={this.state.username} placeholder="USERNAME" />
         <Button action={() => goto(CreateBoard)}>CREATE</Button>
-        <Button action={}>JOIN</Button>
+        <Button>JOIN</Button>
       </Card>
     );
   }
