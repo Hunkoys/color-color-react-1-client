6193f12c66061db6faa57c28ae88664b9dbadf0b
Dominic Victoria <hunkyminik@gmail.com>
2021-04-29 19:43:37 -0700

    Adding emoji in joinscreen

--- src/app/components/OpenGamesListItem.js
+++ src/app/components/OpenGamesListItem.js
@@ -1,4 +1,5 @@
 import { Component } from 'react';
+import { faces } from '../../common/classes';
 import { appendClassName } from '../../common/functions';
 import Game from '../data/Game';
 
@@ -7,7 +8,10 @@ export default class OpenGameItem extends Component {
     const { id, board, host } = Game(this.props.data);
     return (
       <div className={'OpenGamesListItem' + appendClassName(this.props.type)} id={id}>
-        <div className="name">{host.username}</div>
+        <div className="host">
+          <div className="emoji">{faces[host.faceName]}</div>
+          <div className="name">{host.username}</div>
+        </div>
         <div className="board-size">{`${board.size.w} x ${board.size.h}`}</div>
       </div>
     );
